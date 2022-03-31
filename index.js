61cd6769f862c4fdc728c5ad55f296e35e1d0f88
samclarkb <74268703+samclarkb@users.noreply.github.com>
2022-03-31 12:45:58 +0200

    body parser works on the first page

--- index.js
+++ index.js
@@ -1,31 +1,91 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser')
 
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 
+let CSS
+let WAFS
+let BT
+let PWA
+let Home
+
 app.get('/', (req, res) => {
-	res.render('index')
+	res.render('index', { Home })
+})
+
+app.get('/CSS', (req, res) => {
+	res.render('CSS', { CSS })
 })
 
 app.post('/CSS', (req, res) => {
-	res.render('CSS')
+	Home = {
+		name: req.body.name,
+		studentNmbr: req.body.studentNmbr,
+	}
+	res.render('CSS', { CSS })
+})
+
+app.get('/WAFS', (req, res) => {
+	res.render('WAFS', { WAFS })
 })
 
 app.post('/WAFS', (req, res) => {
-	res.render('WAFS')
+	CSS = {
+		teachers: req.body.teachers,
+		date: req.body.date,
+		grade: req.body.grade,
+		quality: req.body.quality,
+		explanation: req.body.explanation,
+		learning: req.body.learning,
+	}
+	res.render('WAFS', { WAFS })
+})
+
+app.get('/BT', (req, res) => {
+	res.render('BT', { BT })
 })
 
 app.post('/BT', (req, res) => {
-	res.render('BT')
+	PWA = {
+		teacher: req.body.teacher,
+		date: req.body.date,
+		grade: req.body.grade,
+		quality: req.body.quality,
+		explanation: req.body.explanation,
+		learning: req.body.learning,
+	}
+	res.render('BT', { BT })
+})
+
+app.get('/PWA', (req, res) => {
+	res.render('PWA', { PWA })
 })
 
 app.post('/PWA', (req, res) => {
-	res.render('PWA')
+	WAFS = {
+		teacher: req.body.teacher,
+		date: req.body.date,
+		grade: req.body.grade,
+		quality: req.body.quality,
+		explanation: req.body.explanation,
+		learning: req.body.learning,
+	}
+	res.render('PWA', { PWA })
 })
 
 app.post('/Succes', (req, res) => {
+	BT = {
+		teacher: req.body.teacher,
+		date: req.body.date,
+		grade: req.body.grade,
+		quality: req.body.quality,
+		explanation: req.body.explanation,
+		learning: req.body.learning,
+	}
 	res.render('Succes')
 })
 
