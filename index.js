bdf3a1c6b7ed07b7f548eb29486728b19adb4717
samclarkb <74268703+samclarkb@users.noreply.github.com>
2022-04-08 13:19:32 +0200

    dotenv for heroku

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const env = require('dotenv')
+const port = process.env.PORT || 5000
 const bodyParser = require('body-parser')
 
 app.use(bodyParser.urlencoded({ extended: false }))
